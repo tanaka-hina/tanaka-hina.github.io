124fb7ff180ac252116cf2e669659ec646f08769
tanaka-hina <tanaka-hina@users.noreply.github.com>
2021-12-02 11:46:31 +0000

    deploy: 2f6ce7c1c0836a4341480e67440ebf62f6f358ec

---
+++ _next/static/2bC9OO7OIpp5dRjmzyUI7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ebb349eff1535cb0.css","static/chunks/pages/index-006c04c94cfab164.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
